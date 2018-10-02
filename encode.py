47bd3041c489f0d1e921dfa708771db539e9c77e
Luuk de Jong <lucasjurgen@gmail.com>
2018-10-02 15:45:57 +0200

    Toevoeging getallentelling en het ordenen in de juiste 'bins' voor het histogram.

--- encode.py
+++ encode.py
@@ -3,15 +3,22 @@
 def encode(text):
     out = ""
     i = 0
+    getal = ""
+    bins = [0]*10
     while i < len(text):
         n = 0
         char = text[i]
         while i < len(text) and text[i] == char:
             n += 1
             i += 1
+        if char.isdigit():
+            getal += char*n
+        elif getal:
+            bins[int(getal)//1000] += 1
+            getal = ""
         if char in "\\01234567890":
             out += "\\"
         out += char
         if n > 1:
             out += str(n)
-    return out
+    return out, bins
