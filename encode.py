9dee005ece3fcd3031f0a6ef59946ed06070f2e7
Luuk de Jong <lucasjurgen@gmail.com>
2018-10-02 14:31:20 +0200

    Toevoeging van encode.py

---
+++ encode.py
@@ -0,0 +1,15 @@
+def encode(text):
+    out = ""
+    i = 0
+    while i < len(text):
+        n = 0
+        char = text[i]
+        while i < len(text) and text[i] == char:
+            n += 1
+            i += 1
+        if char in "\\01234567890":
+            out += "\\"
+        out += char
+        if n > 1:
+            out += str(n)
+    return out
